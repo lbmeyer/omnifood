c4358c5996ada14eb6bee29bb4ea698fb077d922
Louis <louis.sc@gmail.com>
2017-05-30 14:42:15 -0400

    path to waypoints shortened

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
-import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+// import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+import waypoints from 'waypoints/lib/noframework.waypoints';
 import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -11150,6 +11150,8 @@ Object.defineProperty(exports, "__esModule", {
 });
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+// import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
 
 var _jquery = __webpack_require__(0);
 
@@ -11224,6 +11226,8 @@ Object.defineProperty(exports, "__esModule", {
 });
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+// import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
 
 var _jquery = __webpack_require__(0);
 
